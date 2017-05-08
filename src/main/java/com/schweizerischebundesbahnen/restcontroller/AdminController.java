7cb82a973fa130de738ed7162c20d83d79ba0598
aleksandrprendota <prendota@mail.ru>
2017-05-08 16:53:50 +0300

    fixed new upload admin params

--- src/main/java/com/schweizerischebundesbahnen/restcontroller/AdminController.java
+++ src/main/java/com/schweizerischebundesbahnen/restcontroller/AdminController.java
@@ -1,20 +1,36 @@
 package com.schweizerischebundesbahnen.restcontroller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
+import com.schweizerischebundesbahnen.model.Role;
+import com.schweizerischebundesbahnen.model.User;
+import com.schweizerischebundesbahnen.service.api.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Set;
 
 /**
  * Created by aleksandrprendota on 06.05.17.
  */
 @RestController
-@PreAuthorize("hasAuthority('ADMIN')")
 @RequestMapping(value = "/adminparam")
 public class AdminController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(method = RequestMethod.GET)
-    public Boolean isAdmin(){
-        return true;
+    public Boolean isAdmin(Authentication authentication) {
+        User user = userService.findUserByEmail(authentication.getName());
+        Set<Role> roles = user.getRoles();
+        if (roles.size() > 0){
+            for (Role role : roles) {
+                if (role.getRole().equals("ADMIN")){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -7,10 +7,12 @@ $( document ).ready(function() {
         url : '/adminparam',
         contentType: "application/json; charset=utf-8",
         dataType: "json",
-        success: function () {
-            $('#navright').prepend('<li><a href="/admin">Admin</a></li>');
-            $('.chat_window').remove();
-            $('.chat').remove();
+        success: function (response) {
+            if ("true".localeCompare(response) == 0){
+                $('#navright').prepend('<li><a href="/admin">Admin</a></li>');
+                $('.chat_window').remove();
+                $('.chat').remove();
+            }
         },
         error: function () {}
     });
